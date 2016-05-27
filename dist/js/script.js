24020be48fc3696e0995039c01c2711f703bfa81
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-05-27 15:28:54 -0300

    opção todos

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,67 +1,86 @@
-
 $(document).ready(function(){
-	var i = $("#selecionar").val();
-
 	$("#selecionar").change(function(){
 		var i = $("#selecionar").val();
+		$(".radio").show();
+		if (i == "selecionar"){
+			$("#pesquisa").hide();
+			$(".radio").hide();
+			$("#botaoPesquisa").hide();
+		}
 	});
 
 	$("#porNome").click(function(){
-		$("#pesquisa").show();
+		var i = $("#selecionar").val();
+		selectCategoria(i);
+	});
+
+	$("#todos").click(function(){
+		$("#pesquisa").hide();
+		$(".conteudoTodos").show();
+		$("#botaoPesquisa").show();
+		var i = $("#selecionar").val();
+		selectFilter(i);
 	});
 });
 
-var http='http://star-api.herokuapp.com/api/v1/';
+var http="http://star-api.herokuapp.com/api/v1/";
+var azul='min[colorb_v]=-1&max[colorb_v]=0';
+var vermelho='min[colorb_v]=0&max[colorb_v]=1';
+var fracas="min[absmag]=10&max[absmag]=-3";
+var fortes="min[appmag]=-2.5&max[appmag]=6.5";
+var cemAnos="max[distly]=100";	
+var quinhentosAnos = "max[distly]=500";
 
 function selectCategoria(i){
 	if (i != "selecionar"){
-		selectFilter(i);
-	}
-	else {
-		$("#pesquisa").hide();
+		console.log(i);
+		$("#pesquisa").show();
+		$("#botaoPesquisa").show();
 	}
 }
 
 function selectFilter(i){
-		switch (i){
-			case "stars":
-				estrelas(i);
-				break;
-
-			case "exo_planets":
-				exoplanetas(i);
-				break;
-
-			case "local_groups":
-				galaxias(i);
-				break;
-
-			case "open_cluster":
-				open(i);
-				break;
-		}
+	switch(i){
+		case "stars":
+			$("#botaoPesquisa").click(function(){
+				var cor = $("#cor").val();
+				var distancia = $("#distancia").val();
+				var brilho = $("#brilho").val();
+				requisi(i, cor, distancia, brilho);
+			});
+		break;
+	}
 }
 
-function estrelas(i){
+function requisi(i, cor, distancia, brilho){
+	switch(cor){
+		case "azul":
+			var corUrl = azul;
+		break;
 
-	$('#botaoPesquisa').click(function(){
-		var j = $("#pesquisa").val();
-		request(http+i+"/"+j);
-	});
-}
+		case "vermelho":
+			var corUrl = vermelho;
+		break;
+	}
+	switch(distancia){
+		case "cemAnos":
+			var anosUrl = cemAnos;
+		break;
 
-function exoplanetas(i){
-	$('#botaoPesquisa').click(function(){
-		var j = $("#pesquisa").val();
-		request(http+i+"/"+j);
-	});
-}
+		case "quinhentosAnos":
+			var anosUrl = quinhentosAnos;
+		break;
+	}
+	switch(brilho){
+		case "fracas":
+			var brilhoUrl = fracas;
+		break;
 
-function exoplanetas(i){
-	$('#botaoPesquisa').click(function(){
-		var j = $("#pesquisa").val();
-		request(http+i+"/"+j);
-	});
+		case "fortes":
+			var brilhoUrl = fortes;
+		break;
+	}
+	request(http+i+"?"+brilhoUrl+"&"+anosUrl+"&"+corUrl);
 }
 
 function request(url){
