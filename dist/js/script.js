37060bf47190f03cc3ae64ac14c603e187316edd
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-06-01 13:37:39 -0300

    apresentacao

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,81 +1,101 @@
 var http="http://star-api.herokuapp.com/api/v1/";
 var cabecalho={
- listaestrelas:'<table class="table table-striped table-bordered" border="1"><tr><th>Id</th><th>Nome</th><th>Cor</th></tr>',
- listaexo: '<table class="table table-striped table-bordered" border="1"><tr><th>Id</th><th>Nome</th><th>Numplanets</th></tr>'
+ listaestrelas:'<table class="table table-bordered" border="1"><tr><th>Id</th><th>Nome</th><th>Cor</th></th><th>Luminosidade</th></th><th>Distância</th></tr>',
+ listaexo: '<table class="table table-bordered" border="1"><tr><th>Id</th><th>Nome</th><th>Número Planetas</th><th>Distância</th></tr>',
+ listaopen: '<table class="table table-bordered" border="1"><tr><th>Id</th><th>Nome</th><th>Diâmetro</th><th>Distância</th></tr>',
+ listatodos: '<table class="table table-bordered" border="1"><tr><th>Id</th><th>Nome</th></tr>'
 }
 
 $(document).ready(function(){
+
 	$("#selecionar").change(function(){
 		conteudo = recebeConteudo();
-
 		if (conteudo == "selecionar"){
+			$('.conteudo').css('backgroundImage','url(constelacao.jpg)');
 			esconde(['#pesquisa','.radio','#botaoPesquisa','.conteudoTodos']);
 		}
-
-		else mostra(['.radio']);
+		else {
+			$("#pesquisa").val('');
+			esconde(['#conteudo']);
+			mostra(['.radio']);
+			if ($("#todos").is(':checked')){
+				teste();
+			}
+			if (conteudo == "stars"){
+				$('.conteudo').css('backgroundImage','url(estrelas.jpg)');
+			}
+			else if (conteudo == "exo_planets"){
+				$('.conteudo').css('backgroundImage','url(exoplanet.jpg)');
+				//mostraExo();
+			}
+			else if (conteudo == "open_cluster"){
+				$('.conteudo').css('backgroundImage','url(openstar.jpg)');
+				//mostraOpen();
+			}
+		}
 	});
 
 	$("#porNome").click(function(){
 		conteudo = recebeConteudo();
-		selectCategoria(conteudo);
 		limpaSelect();
 		esconde(['.conteudoTodos']);
 
 		$("#selecionar").change(function(){
 			conteudo = recebeConteudo();
-			selectCategoria(conteudo);
 		});
+		selectCategoria(conteudo);
 	});
 
 	$("#todos").click(function(){
-		conteudo = recebeConteudo();
 		limpaSelect();
 		esconde(['#pesquisa','#conteudo']);
 		mostra(['#botaoPesquisa']);
-		
-		if (conteudo == "stars"){
-			mostra([".estrelasTodos",".conteudoTodos"]);
-			esconde([".exoplanetas",".open_cluster"]);
-		}
-		else if (conteudo == "exo_planets"){
-			mostra(['.exoplanetas','.conteudoTodos']);
-			esconde(['.estrelasTodos','.open_cluster']);
-		}
-		else if (conteudo == "open_cluster"){
-			mostra(['.open_cluster','.conteudoTodos']);
-			esconde(['.exoplanetas','.estrelasTodos']);
-		}
+		teste();
 	});
 
 	$('#botaoPesquisa').click(function(){
-		var i = $("#selecionar").val();
+		conteudo = recebeConteudo();
 		var j = $("#pesquisa").val();
-		if (j != ""){
-			request(http+i+"/"+j);
-		}
 
-		else if (j == ""){
-			if (i == "stars"){
+		if (j == ""){
+			if (conteudo == "stars"){
 				var cor = $("#cor").val();
 				var distancia = $("#distancia").val();
 				var brilho = $("#brilho").val();
-				testeUrlEstrelas(i, cor, distancia, brilho);
+				testeUrlEstrelas(conteudo, cor, distancia, brilho);
 			}
 
-			else if (i == "exo_planets"){
+			else if (conteudo == "exo_planets"){
 				var numplanets = $("#numplanetas").val();
 				var distancia = $("#distance").val();
-				testeUrlExoplanets(i, numplanets, distancia);
+				testeUrlExoplanets(conteudo, numplanets, distancia);
 			}
 
-			else if (i == "open_cluster"){
+			else if (conteudo == "open_cluster"){
 				var diametro = $("#diametroOp").val();
 				var distanciaOp = $("#distanciaOp").val();
-				testeUrlOp(i, diametro, distanciaOp);
+				testeUrlOp(conteudo, diametro, distanciaOp);
 			}
 		}
+
+		else {porNome();}
 	});
 });
+
+function teste(){
+	conteudo = recebeConteudo();
+	if (conteudo == "stars") mostraEstrela();
+	else if (conteudo == "open_cluster") mostraOpen();
+	else if (conteudo == "exo_planets") mostraExo();
+}
+function porNome(){
+	conteudo = recebeConteudo();
+	var j = $("#pesquisa").val();
+	var nbsp = j.replace('%20',' ');
+	if (j != ""){
+		request(http+conteudo+"/"+j);
+	}
+}
 
 function recebeConteudo(){
 	var conteudo;
@@ -87,8 +107,8 @@ function recebeConteudo(){
 }
 
 
-function selectCategoria(i){
-	if (i != "selecionar"){
+function selectCategoria(conteudo){
+	if (conteudo != "selecionar"){
 		$("#pesquisa").show();
 		$("#botaoPesquisa").show();
 	}
@@ -112,7 +132,6 @@ function testeValor(distancia, cor, brilho, distanciaOp, diametro){
 		var cor = splitValue(cor);
 		var brilho = splitValue(brilho);
 		data = [cor, brilho]
-		console.log (data);
 	}
 	else if (distanciaOp != "" || diametro != ""){
 		var distanciaOp = splitValue(distanciaOp);
@@ -150,26 +169,33 @@ function testeUrlEstrelas(i, cor, distancia, brilho){
 }
 
 function testeUrlOp(i, diametro, distanciaOp){
+	var distUrl;
+	var diamUrl;
 	data = testeValor('','', '', distanciaOp, diametro);
 
-	if (data[1].min == "") {
-		var diamUrl = "min[diam]="+data[1].max;
-	}
-
-	else if (data[1].min != ""){
-		var diamUrl = "min[diam]="+data[1].min+"&max[diam]="+data[1].max;
-	}
-
 	if (data[0].min == ""){
-		var distUrl = "min[distly]="+data[0].max;
+		if (data[0].max == "distanciaOp") distUrl = "undefined";
+		else if (data[0].max == "60001") distUrl = "min[distly]="+data[0].max;
 	}
-	else if(data[0].min!= ""){
-		var distUrl = "min[distly]="+data[0].min+"&max[distly]="+data[0].max;
+
+	else if(data[0].min!= "" && data[0].max != "" ){
+		distUrl = "min[distly]="+data[0].min+"&max[distly]="+data[0].max;
+	}
+
+	if (data[1].min == ""){
+		if (data[1].max == "diametroOp") diamUrl = "undefined";
+		else if (data[1].max == "100") diamUrl = "min[diam]="+data[1].max;
+	}
+
+	else if (data[1].min != "" && data[1].max != "" ){
+		 diamUrl = "min[diam]="+data[1].min+"&max[diam]="+data[1].max;
 	}
 	request(http+i+"?"+diamUrl+"&"+distUrl);
 }
 
+
 function testeUrlExoplanets(i, numplanets, distancia){
+	var distUrl;
 	if (numplanets != "exo_planets"){
 		var numPlanets = parseInt(numplanets);
 		if (numplanets == 1){
@@ -184,60 +210,62 @@ function testeUrlExoplanets(i, numplanets, distancia){
 	}
 
 	data = testeValor(distancia, '', '', '', '');
-	if (data[0] == ""){
-		var distUrl = "min[distance]="+data[1];
+		console.log(data[0].min);
+		console.log(data[0].max);
+	if (data[0].min == ""){
+		if (data[0].max == "1001")distUrl = "min[distance]="+data[0].max;
+		else if (data[0].max == "distancia") distUrl = "undefined"; 
 	}
-	else if(data[0] != ""){
-		var distUrl = "min[distance]="+data[0]+"&max[distance]="+data[1];
-	}
+
+	else if(data[0].min != "") distUrl = "min[distance]="+data[0].min+"&max[distance]="+data[0].max;
+
 	request(http+i+"?"+numUrl+"&"+distUrl);
 }
 
 
-
 function request(url){
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function (){
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-			var myArr = JSON.parse(xmlhttp.responseText);
-			tabela(myArr);
+	$.getJSON(url, function (myArr){
+	conteudo = recebeConteudo();
+	var j = $("#pesquisa").val();
+	var result = "";
+		/*var oi = Object.keys(myArr.lum).length;
+		console.log(oi);'*/
+	if (j != ""){
+		if (conteudo == "stars"){
+			result+= cabecalho.listaestrelas; 
+			result += '<tr><td>'+ myArr.id + '</td>';
+			result += '<td>'+ myArr.label + '</td>';
+			result += '<td>'+ myArr.colorb_v + '</td>';
+			result += '<td>'+ myArr.lum + '</td>';
+			result += '<td>'+ myArr.distly + '</td></tr>';
+		}
+
+		else if (conteudo == "exo_planets"){
+			result+= cabecalho.listaexo;
+			result += '<tr><td>'+ myArr.id + '</td>';
+			result += '<td>'+ myArr.label + '</td>';
+			result += '<td>'+ myArr.numplanets + '</td>';
+			result += '<td>'+ myArr.distance + '</td></tr>'
+		}
+
+		else if (conteudo == "open_cluster"){
+			result+= cabecalho.listaopen;
+			result += '<tr><td>'+ myArr.id + '</td>';
+			result += '<td>'+ myArr.label + '</td>';
+			result += '<td>'+ myArr.diam + '</td>';
+			result += '<td>'+ myArr.distly + '</td></tr>';
 		}
 	}
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send();
-}
-
-function tabela(myArr){
-	var result = "";
-	conteudo = recebeConteudo();
-	if (conteudo == "stars"){
-		result+= cabecalho.listaestrelas; 
-		result += '<tr><td>'+ myArr.id + '</td>';
-		result += '<td>'+ myArr.label + '</td>';
-		result += '<td>'+ myArr.colorb_v + '</td></tr>'
-	}
-	else if (conteudo == "exo_planets"){
-		result+= cabecalho.listaexo;
-		result += '<tr><td>'+ myArr.id + '</td>';
-		result += '<td>'+ myArr.label + '</td>';
-		result += '<td>'+ myArr.numplanets + '</td></tr>'
+	else{
+		result+= cabecalho.listatodos;
+		for (i=0; i < 10; i++){
+			result += '<tr><td>'+ myArr[i].id + '</td>';
+			result += '<td>'+ myArr[i].label + '</td>';
+		}
 	}
 	$("#conteudo").show();
-
-	document.getElementById("conteudo").innerHTML = result;		
-}
-
-
-function esconde (array){
-	for (var x=0; x<array.length; x++){
-		$(array[x]).hide();
-	}
-}
-
-function mostra (array){
-	for (var x=0; x<array.length; x++){
-		$(array[x]).show();
-	}
+	$('#conteudo').html(result);	
+	})
 }
 
 function limpaSelect(){
@@ -249,4 +277,31 @@ function limpaSelect(){
 	$("#diametroOp").val("diametroOp");
 	$("#distanciaOp").val("distanciaOp");
 	$("#pesquisa").val('');
+}
+
+function mostraEstrela(){
+	mostra([".estrelasTodos",".conteudoTodos"]);
+	esconde([".exoplanetas",".open_cluster"]);
+}
+
+function mostraExo(){
+	mostra(['.exoplanetas','.conteudoTodos']);
+	esconde(['.estrelasTodos','.open_cluster']);
+}
+
+function mostraOpen(){
+	mostra(['.open_cluster','.conteudoTodos']);
+	esconde(['.exoplanetas','.estrelasTodos']);
+}
+
+function esconde (array){
+	for (var x=0; x<array.length; x++){
+		$(array[x]).hide();
+	}
+}
+
+function mostra (array){
+	for (var x=0; x<array.length; x++){
+		$(array[x]).show();
+	}
 }
